fc26954f1b0c16aa5946a33ef49c5df5e718674e
RiyaKulkarni16 <kulkarniriya28@gmail.com>
2025-04-15 17:11:57 +0530

    Add files via upload

---
+++ TodoApp/src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Image from "next/image";
+import Header from "@/components/Header";
+import Todo from "@/components/Todo";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Todo />
+    </div>
+  )
+}
